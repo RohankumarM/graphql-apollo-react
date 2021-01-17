de2b16513c22551256a633195ef1dd9784114b76
Rohankumar Maisuria <rmaisuria@horizon.csueastbay.edu>
2021-01-17 23:37:52 +0530

    graphql is amazing

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 //allow cross origin requests
 app.use(cors());
 
-moogoose.connect("mongodb+srv://rohanM:rohan123@cluster0.vjkgo.mongodb.net/graphql_tut?retryWrites=true&w=majority",
+moogoose.connect("mongodb+srv://<username>:<password>@cluster0.vjkgo.mongodb.net/graphql_tut?retryWrites=true&w=majority",
 {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("Connected to db"))
 .catch(err => console.log(err))
